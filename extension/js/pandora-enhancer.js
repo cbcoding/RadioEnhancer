378e5cca1c67c4103ae536bfb91d335e5afdaa52
Brandon <antizeph@gmail.com>
2011-10-21 19:08:00 -0400

    promobox and stationlist extension

--- extension/js/pandora-enhancer.js
+++ extension/js/pandora-enhancer.js
@@ -2,25 +2,32 @@ console.log("Pandora Enhancer loaded.");
 
 var hideAds = function()
 {
-	jQuery("#mainContainer").css({"background-image":"none !important", "background-color":"#09102a"});
-	jQuery("#mainContentContainer").css("float", "none !important");
+    jQuery("#mainContainer").css({"background-image":"none !important", "background-color":"#09102a"});
+    jQuery("#mainContentContainer").css("float", "none !important");
 };
 
+var extendStationList = function()
+{
+    jQuery(".stationListHolder").css("height", "665px !important");
+    jQuery(".jspContainer").css("height", "655px !important");
+}
+
 chrome.extension.sendRequest({}, function(response) { //json
-	//console.log("pandora-enhancer.js response: " + response);
+    //console.log("pandora-enhancer.js response: " + response);
 });
 
 jQuery(".still_listening").livequery(function(){
-	console.log("still listening? doesn't matter. there's no more 40 hour limit!");
-	jQuery(this).click();
+    console.log("still listening? doesn't matter. there's no more 40 hour limit!");
+    jQuery(this).click();
 });
 
 jQuery("#mainContentContainer, #mainContainer").livequery(function(){
-	hideAds();
+    hideAds();
 });
 
-jQuery("#ad_container, #ad_frame, #adContainer").livequery(function(){
-	jQuery(this).remove();
+jQuery("#ad_container, #ad_frame, #adContainer, .contextual_help_container, #promobox").livequery(function(){
+    jQuery(this).remove();
 });
 
 hideAds();
+extendStationList();
