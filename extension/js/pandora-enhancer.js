5e533ab86152b4a5ee3ca1eb2e08a54a5df89c4b
Curt Hostetter <hostetcl@gmail.com>
2011-10-21 17:59:31 -0400

    Simplifies removal and uses livequery extension to remove ads as soon as they're created.

--- extension/js/pandora-enhancer.js
+++ extension/js/pandora-enhancer.js
@@ -1,5 +1,15 @@
 console.log("Pandora Ad Remover Loaded");
 
+var hideAds = function()
+{
+	console.log('Hiding ads...');
+	jQuery("#ad_frame").css({"display":"none !important"});
+	jQuery("#ad_container").css({"display":"none !important"});
+	jQuery("#mainContainer").css({"background-image":"none !important", "background-color":"#09102a"});
+	jQuery("#mainContentContainer").css("float", "none !important");
+	console.log('Done.');
+};
+
 chrome.extension.sendRequest({}, function(response) { //json
 	//console.log("pandora-enhancer.js response: " + response);
 });
@@ -7,28 +17,11 @@ chrome.extension.sendRequest({}, function(response) { //json
 //set an interval, because pandora sideloads its player after the DOM is done loading
 //pandora also changes advertisements often, so this should keep the ads hidden
 
-
-/*
-//i'll see if this works later
-jQuery("#ad_frame, #ad_container").live(function(){
-	$(this).css({"display":"none !important"});
-});
-*/
-
-jQuery(".still_listening").live('load', function(){
+jQuery(".still_listening").livequery(function(){
 	console.log("still listening? doesn't matter. there's no more 40 hour limit!");
 	jQuery(this).click();
 });
 
-//TODO: figure out the event they use to load new ads
-
-/**/
-setInterval(function(){
-	jQuery("#ad_frame").css({"display":"none !important"});
-	jQuery("#ad_container").css({"display":"none !important"});
-	//console.log("ad frame should be hidden");
-
-	jQuery("#mainContainer").css({"background-image":"none !important", "background-color":"#09102a"});
-	jQuery("#mainContentContainer").css("float", "none !important");
-	//console.log("mainContainer bg should be set to none");
-},250);
+jQuery("#mainContentContainer, #mainContainer, #ad_container, #ad_frame").livequery(function(){
+	hideAds();
+});
